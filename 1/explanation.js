4d77c7b8e5412c1b152deaa861001b8c8e9528de
marcefalb <marcefalb@gmail.com>
2024-01-21 17:20:08 +0300

    add explanation.js

---
+++ 1/explanation.js
@@ -0,0 +1,11 @@
+// let name = "Masha";
+// const age = 25;
+// var greeting = "Hello, world!";
+//
+// let age = 25;  // Это число
+// age = "25";    // Теперь это строка
+//
+// let myVar;
+// let myvar;
+//
+// const PI = 3.14;
